8a403de933a3bf85864bd42bd98e24f94a69b017
Kenneth Collins <kennethgcollins@outlook.com>
2025-07-23 11:55:49 -0400

    First attempt at fixed learning rate schedule with AdamW

--- cellpose/train.py
+++ cellpose/train.py
@@ -398,16 +398,7 @@ def train_seg(net, train_data=None, train_labels=None, train_files=None,
     nimg_test_per_epoch = nimg_test if nimg_test_per_epoch is None else nimg_test_per_epoch
 
     # learning rate schedule
-    LR = np.linspace(0, learning_rate, 10)
-    LR = np.append(LR, learning_rate * np.ones(max(0, n_epochs - 10)))
-    if n_epochs > 300:
-        LR = LR[:-100]
-        for i in range(10):
-            LR = np.append(LR, LR[-1] / 2 * np.ones(10))
-    elif n_epochs > 99:
-        LR = LR[:-50]
-        for i in range(10):
-            LR = np.append(LR, LR[-1] / 2 * np.ones(5))
+    LR = [learning_rate for i in range(n_epochs)]
 
     train_logger.info(f">>> n_epochs={n_epochs}, n_train={nimg}, n_test={nimg_test}")
     train_logger.info(
